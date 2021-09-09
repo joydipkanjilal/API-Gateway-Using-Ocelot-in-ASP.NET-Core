b2c3b27ea3f144b808d2ecd9f5be30e27670192e
Joydip Kanjilal <joydipkanjilal@yahoo.com>
2021-09-09 18:22:37 +0530

    Initial commit

---
+++ OrderProcessing.Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OrderProcessing.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public DateTime Order_Date { get; set; }
+        public Guid CustomerId { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+        Processing, Shipped, Delivered
+    }
+}
